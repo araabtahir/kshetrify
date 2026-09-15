ee80bbda74bb4a7f11a0314753246c2158014cd2
ARAB TAHIR <araabtahir@gmail.com>
2026-09-16 02:22:46 +0530

    Use lightweight PaddleOCR mobile models

--- ai_service/main.py
+++ ai_service/main.py
@@ -9,6 +9,8 @@ app = FastAPI(title="Kshetrify AI Service")
 
 ocr = PaddleOCR(
     lang="en",
+    text_detection_model_name="PP-OCRv5_mobile_det",
+    text_recognition_model_name="en_PP-OCRv5_mobile_rec",
     use_doc_orientation_classify=False,
     use_doc_unwarping=False,
     use_textline_orientation=False
